9eda95b5e81c1552ff383cc7c45e38859ef1e50d
Edney Siqueira <edneyfs@gmail.com>
2020-08-24 15:09:06 -0300

    TEstando a primeira classe de cominio - Categoria

--- src/main/java/com/efs/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/efs/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.efs.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.efs.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource implements Resource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST esta funcionando";
+	public List<Categoria> listar() {
+		Categoria c1 = new Categoria(1, "Informativa");
+		Categoria c2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(c1);
+		lista.add(c2);
+		
+		return lista;
 	}
 
 }
