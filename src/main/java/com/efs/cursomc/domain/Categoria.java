a5e12caa384eaa6735747a61aaced923291ca3ef
Edney Siqueira <edneyfs@gmail.com>
2020-08-24 16:23:37 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/efs/cursomc/domain/Categoria.java
+++ src/main/java/com/efs/cursomc/domain/Categoria.java
@@ -2,12 +2,21 @@ package com.efs.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -4546630630682245765L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 
