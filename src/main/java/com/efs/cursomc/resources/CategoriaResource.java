9cb280947e1998a0a619fd67afec179ec870e823
Edney Siqueira <edneyfs@gmail.com>
2020-08-25 22:00:09 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/efs/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/efs/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.efs.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.efs.cursomc.domain.Categoria;
 import com.efs.cursomc.services.CategoriaService;
@@ -18,11 +22,18 @@ public class CategoriaResource {
 	private CategoriaService service;
 	
 	
-	// @PathVariable faz a ligação com o parametro value={id}
-	//ResponseEntity - add informaçoes de uma comunicaçao rest http
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<Categoria> find(@PathVariable Integer id) { 
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) { // @PathVariable faz a ligação com o parametro value={id}
 		Categoria obj = service.buscar(id);
+		//ResponseEntity - add informaçoes de uma comunicaçao rest http
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) { // (@RequestBody converte o JSON neste objeto
+		obj = service.insert(obj);
+		//pega a URI para pesquisa este novo objeto (boa pratica)
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/efs/cursomc/services/CategoriaService.java
+++ src/main/java/com/efs/cursomc/services/CategoriaService.java
@@ -24,4 +24,8 @@ public class CategoriaService {
 				() -> new ObjectNotFoundException(
 						"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
+
+	public Categoria insert(Categoria obj) {
+		return repo.save(obj);
+	}
 }
